b6102bf5156a1015fd89a6f9d3b00e87f59924bd
Christopher Paepe <cmpsbh@gmail.com>
2013-12-02 18:55:47 -0500

    Added method new and create to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
 
 	def new
+		@user = User.new
 	end
 
+	def create
+		@user = User.new(user_params)
+		# if @user.save
+		# 	redirect_to projects_url, :notice => "Signed Up!"
+		# else
+		# 	render "new"
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation, :name)
 end
