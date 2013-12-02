d59f318899044037a93890e689ec0f5986f8c28c
Christopher Paepe <cmpsbh@gmail.com>
2013-12-02 17:59:53 -0500

    Prohject initialize, User controller created. Still empty.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrowdFunder::Application.config.secret_key_base = '92caf5de335b08c177563bcbe00a0dba8edd7c5280c7246ae880676dd966e38ebcf8ff8bb16ae543270cd51c5d6c7c7ceaa8c7b29aa0b36fb8944a88b99a7e49'
