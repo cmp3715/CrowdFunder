981fb5624815fd03faf9d85611d51ccf4c3cca10
Christopher Paepe <cmpsbh@gmail.com>
2013-12-03 12:03:15 -0500

    Fixed the Schema and have the Sign Up page running

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202230037) do
+ActiveRecord::Schema.define(version: 20131203165542) do
 
   create_table "users", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "email"
+    t.string "password_digest"
+    t.string "name"
   end
 
 end
